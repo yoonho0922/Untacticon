1f091866517200672a51ff8255df1380436a9bc0
yoonho <yoonho0922@naver.com>
2020-11-24 21:21:11 +0900

    user window sleep detect integration

--- detector.py
+++ detector.py
@@ -1,66 +1,118 @@
 from imutils.video import VideoStream
 from imutils import face_utils
 import imutils
-import time
 import dlib
 import cv2
-import threading
 import time
 from queue import Queue
-
-def detector(q):
-    # 모델 불러오기
-    detector = dlib.get_frontal_face_detector()
-    predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
-
-    # video stream 시작
-    vs = VideoStream(0).start()
-    time.sleep(2.0)
-
-    while True:
-        frame = vs.read()
-        frame = imutils.resize(frame, width=400)
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-        rects = detector(gray, 0)
-
-        for rect in rects:
-            shape = predictor(gray, rect)
-            shape = face_utils.shape_to_np(shape)
-
-            for (x, y) in shape:
-                cv2.circle(frame, (x, y), 1, (0, 0, 255), -1)
-
-        cv2.imshow("webcam", frame)
-        key = cv2.waitKey(1) & 0xFF
+from scipy.spatial import distance as dist
 
 
+class MyDetector:
 
-        if key == ord("1"):
-            print(1)
-            q.put("1번")
-        if key == ord("2"):
-            print(2)
-            q.put("2번")
+    # sleep 관련
+    STATE = "normal"
+    COUNTER = 0
+    TOTAL = 0
 
-        if key == ord("q"):
-            break
+    EYE_AR_THRESH = 0.3
+    SLEEP_CONSEC_FRAMES = 60
+
+    (lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
+    (rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
+
+    def get_ear(self, eye):
+        A = dist.euclidean(eye[1], eye[5])
+        B = dist.euclidean(eye[2], eye[4])
+        C = dist.euclidean(eye[0], eye[3])
+        ear = (A + B) / (2.0 * C)
+        return ear
+
+    def detect_sleep(self, shape, frame, state, state_changed):
+        leftEye = shape[self.lStart: self.lEnd]
+        rightEye = shape[self.rStart:self.rEnd]
+        leftEAR = self.get_ear(leftEye)
+        rightEAR = self.get_ear(rightEye)
+
+        ear = (leftEAR + rightEAR) / 2.0
+
+        leftEyeHull = cv2.convexHull(leftEye)
+        rightEyeHull = cv2.convexHull(rightEye)
+
+        # 졸음 감지
+        if ear < self.EYE_AR_THRESH:  # 눈을 감았을 때
+            self.COUNTER += 1
+
+            # 눈을 계속 감고 있는 경우 -> 졸음이라고 판단
+            if self.COUNTER >= self.SLEEP_CONSEC_FRAMES:
+                self.STATE = "sleep!"
+                state = 3
+                state_changed.emit('{}'.format(state))
+
+        else:  # 눈을 떴을 때
+            self.COUNTER = 0
+            self.STATE = "normal"
+            state = 1
+            state_changed.emit('{}'.format(state))
+
+        # 화면에 표시
+        # cv2.putText(frame, "COUNTER: {}".format(self.COUNTER), (10, 30),
+        #             cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+        # cv2.putText(frame, "USER STATE: {}".format(self.STATE), (300, 30),
+        #             cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+
+    def video(self, state, state_changed):
+
+
+
+        # 모델 불러오기
+        print('load model...')
+        detector = dlib.get_frontal_face_detector()
+        predictor = dlib.shape_predictor("public/shape_predictor_68_face_landmarks.dat")
+
+        # video stream 시작
+        print('start video stream')
+        vs = VideoStream(0).start()
+        time.sleep(2.0)
+
+        print('start detecting')
+        while True:
+
+            frame = vs.read()
+            frame = imutils.resize(frame, width=600)
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+            rects = detector(gray, 0)
+
+            for rect in rects:
+                shape = predictor(gray, rect)
+                shape = face_utils.shape_to_np(shape)
+
+                self.detect_sleep(shape, frame, state, state_changed)
+
+
+
+            cv2.imshow("webcam", frame)
+            key = cv2.waitKey(1) & 0xFF
+
+            if key == ord("1"):
+                state = 1
+                state_changed.emit('{}'.format(state))
+            if key == ord("2"):
+                sec = 2
+                state_changed.emit('{}'.format(state))
+
+            if key == ord("q"):
+                break
+
+
 
 ## __main__
 
 if __name__=='__main__':
     q = Queue()
+    # t = threading.Thread(target=MyDetector.video(q), args=(q,))
+    # t.start()
+    md = MyDetector()
+    md.video(q)
 
-    t = threading.Thread(target=sum, args=(q,))
-    t.start()
-
-    i = 0
-    while True:
-        print("main thread {}".format(i))
-        i = i + 1
-        print(q.empty())
-        if q.empty() == False:
-            data = q.get()
-            print(data)
-
-        time.sleep(1)
--- user_window.py
+++ user_window.py
@@ -1,20 +1,21 @@
-import sys
-import time
-from PyQt5.QtWidgets import QApplication, QWidget, QDesktopWidget, QPushButton,QVBoxLayout,QLabel,QHBoxLayout
-from PyQt5.QtGui import QIcon
-from PyQt5.QtGui import QPixmap
-from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import*
 from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+import cv2
+from imutils.video import VideoStream
+from imutils import face_utils
+import imutils
+import time
+import dlib
+import cv2
+import threading
+import time
+from PyQt5.QtGui import QPixmap
+import detector
 
+class MyMainGUI(QDialog):
+    def __init__(self, parent=None):
+        super().__init__(parent)
 
-
-class MyApp(QWidget):
-    def __init__(self):
-        super().__init__()
-        self.initUI()
-
-    def initUI(self):
         self.neutral=QPixmap('public/neutral.png')
         self.question=QPixmap('public/question_sizeup.png')
         self.question=self.question.scaledToWidth(200)
@@ -24,77 +25,130 @@ class MyApp(QWidget):
         self.yes=QPixmap('public/yes.png')
         self.no=QPixmap('public/no.png')
 
-
-
         self.lbl_img=QLabel()
         self.lbl_img.setPixmap(self.neutral)
+        self.btn1 = QPushButton("Start", self)
+
+        vbox = QVBoxLayout()
+        vbox.addWidget(self.lbl_img)
+        vbox.addWidget(self.btn1)
+        self.setLayout(vbox)
+
+        self.setGeometry(100, 50, 300, 300)
+
+class Test:
+    def __init__(self):
+        name = ""
 
 
+class MyMain(MyMainGUI):
+    add_sec_signal = pyqtSignal()
+    send_instance_singal = pyqtSignal("PyQt_PyObject")
 
+    def __init__(self, parent=None):
+        super().__init__(parent)
 
-        btn=QPushButton(self)
-        self.btn2 = QPushButton()
-        self.btn2.setText("back")
-        self.bt2=0
-        btn.setIcon(QIcon('public/question.png'))
-        btn.clicked.connect(self.btn_clicked)
-        self.btn2.clicked.connect(self.btn2_clicked)
+        self.btn1.clicked.connect(self.start)
 
+        self.th = Worker(parent=self)
+        self.th.sec_changed.connect(self.state_update)  # custom signal from worker thread to main thread
 
-        self.vbox=QVBoxLayout()
-
-        self.vbox.addWidget(self.lbl_img)
-        self.vbox.addWidget(btn)
-
-
-
-
-        self.setLayout(self.vbox)
-        self.setWindowTitle('Untacticon UI')
-        self.resize(200,300)
-        self.center()
         self.show()
 
+    @pyqtSlot()
+    def start(self):
+        self.th.start()
+        self.th.working = True
 
-
-
-    def btn_clicked(self):
-        #QMessageBox.about(self,"message","질문하였습니다.")
-        self.lbl_img.setPixmap(self.question)
-        self.bt2=self.vbox.addWidget(self.btn2)
-
-    def btn2_clicked(self):
-        self.lbl_img.setPixmap(self.neutral)
-        self.vbox.itemAt(2).widget().setParent(None)
-
-    def keyPressEvent(self, e):
-        if e.key()==Qt.Key_1:
+    @pyqtSlot(str)
+    def state_update(self, msg):
+        if msg == "1":
             self.lbl_img.setPixmap(self.neutral)
-        elif e.key()==Qt.Key_2:
+        elif msg == "2":
             self.lbl_img.setPixmap(self.doubt)
-        elif e.key()==Qt.Key_3:
+        elif msg == "3":
             self.lbl_img.setPixmap(self.tired)
-        elif e.key()==Qt.Key_4:
+        elif msg == "4":
             self.lbl_img.setPixmap(self.being_left)
-        elif e.key()==Qt.Key_5:
+        elif msg == "5":
             self.lbl_img.setPixmap(self.yes)
-        elif e.key()==Qt.Key_6:
+        elif msg == "6":
             self.lbl_img.setPixmap(self.no)
 
 
 
+class Worker(QThread):
+    sec_changed = pyqtSignal(str)
+
+    def __init__(self, state=0, parent=None):
+        super().__init__()
+        self.main = parent
+        self.working = True
+        self.state = state
+
+        # self.main.add_sec_signal.connect(self.add_sec)   # 이것도 작동함. # custom signal from main thread to worker thread
+
+    def __del__(self):
+        print(".... end thread.....")
+        self.wait()
+
+    def run(self):
+
+        md = detector.MyDetector()
+        md.video(self.state, self.sec_changed)
+        # print('load model...')
+        # detector = dlib.get_frontal_face_detector()
+        # # predictor = dlib.shape_predictor("public/shape_predictor_68_face_landmarks.dat")
+        #
+        # print('start video stream')
+        # vs = VideoStream(0).start()
+        # time.sleep(2.0)
+        #
+        # print('start detecting')
+        # while self.working:
+        #         frame = vs.read()
+        #         frame = imutils.resize(frame, width=400)
+        #         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        #
+        #         rects = detector(gray, 0)
+        #
+        #         # for rect in rects:
+        #             # shape = predictor(gray, rect)
+        #             # shape = face_utils.shape_to_np(shape)
+        #             #
+        #             # for (x, y) in shape:
+        #             #     cv2.circle(frame, (x, y), 1, (0, 0, 255), -1)
+        #
+        #         cv2.imshow("webcam", frame)
+        #         key = cv2.waitKey(1) & 0xFF
+        #
+        #         if key == ord("1"):
+        #             self.sec = 1
+        #             self.sec_changed.emit('{}'.format(self.sec))
+        #         if key == ord("2"):
+        #             self.sec = 2
+        #             self.sec_changed.emit('{}'.format(self.sec))
+        #         if key == ord("3"):
+        #             self.sec = 3
+        #             self.sec_changed.emit('{}'.format(self.sec))
+        #         if key == ord("4"):
+        #             self.sec = 4
+        #             self.sec_changed.emit('{}'.format(self.sec))
+        #         if key == ord("5"):
+        #             self.sec = 5
+        #             self.sec_changed.emit('{}'.format(self.sec))
+        #         if key == ord("6"):
+        #             self.sec = 6
+        #             self.sec_changed.emit('{}'.format(self.sec))
+        #
+        #         if key == ord("q"):
+        #             break
 
 
 
+if __name__ == "__main__":
+    import sys
 
-
-    def center(self):
-        qr=self.frameGeometry()
-        cp=QDesktopWidget().availableGeometry().center()
-        qr.moveCenter(cp)
-        self.move(qr.topLeft())
-
-if __name__=='__main__':
-    app=QApplication(sys.argv)
-    ex=MyApp()
-    sys.exit(app.exec_())
+    app = QApplication(sys.argv)
+    form = MyMain()
+    app.exec_()
