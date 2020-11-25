016fea66544f930941636a3f0b45777ad8c0db77
yoonho <yoonho0922@naver.com>
2020-11-25 15:43:27 +0900

    user window detect UI 추가

--- user_window.py
+++ user_window.py
@@ -1,40 +1,48 @@
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
-import cv2
-from imutils.video import VideoStream
-from imutils import face_utils
-import imutils
-import time
-import dlib
-import cv2
-import threading
-import time
 from PyQt5.QtGui import QPixmap
 import detector
+from PyQt5.QtGui import QIcon
 
 class MyMainGUI(QDialog):
     def __init__(self, parent=None):
         super().__init__(parent)
 
+        self.detect_on=QPixmap('public/detect_on.png').scaledToWidth(10)
+        self.detect_off=QPixmap('public/detect_off.png')
+
         self.neutral=QPixmap('public/neutral.png')
         self.question=QPixmap('public/question_sizeup.png')
-        self.question=self.question.scaledToWidth(200)
         self.doubt = QPixmap('public/doubt.png')
         self.tired=QPixmap('public/tired.png')
         self.being_left=QPixmap('public/being left.png')
         self.yes=QPixmap('public/yes.png')
         self.no=QPixmap('public/no.png')
 
-        self.lbl_img=QLabel()
-        self.lbl_img.setPixmap(self.neutral)
-        self.btn1 = QPushButton("Start", self)
+        # 얼굴인식 여부
+        self.detectDot = QLabel()
+        self.detectDot.resize(10,10)
+        self.detectDot.setPixmap(self.detect_off)
+        self.detectText = QLabel('인식안됨', self)
+
+        hbox = QHBoxLayout()
+        hbox.addWidget(self.detectDot)
+        hbox.addWidget(self.detectText)
+
+        # 사용자 이모티콘
+        self.userEmoticon = QLabel()
+        self.userEmoticon.setPixmap(self.neutral)
+
+        # 질문 버튼
+        self.questionBtn = QPushButton(self)
+        self.questionBtn.setIcon(QIcon('public/question.png'))
 
         vbox = QVBoxLayout()
-        vbox.addWidget(self.lbl_img)
-        vbox.addWidget(self.btn1)
+        vbox.addLayout(hbox)
+        vbox.addWidget(self.userEmoticon)
+        vbox.addWidget(self.questionBtn)
         self.setLayout(vbox)
-
-        self.setGeometry(100, 50, 300, 300)
+        self.setGeometry(100, 50, 200, 300)
 
 class Test:
     def __init__(self):
@@ -48,32 +56,28 @@ class MyMain(MyMainGUI):
     def __init__(self, parent=None):
         super().__init__(parent)
 
-        self.btn1.clicked.connect(self.start)
-
+        # video 스레드
         self.th = Worker(parent=self)
         self.th.sec_changed.connect(self.state_update)  # custom signal from worker thread to main thread
-
-        self.show()
-
-    @pyqtSlot()
-    def start(self):
         self.th.start()
         self.th.working = True
+        
+        self.show()
 
     @pyqtSlot(str)
     def state_update(self, msg):
         if msg == "1":
-            self.lbl_img.setPixmap(self.neutral)
+            self.userEmoticon.setPixmap(self.neutral)
         elif msg == "2":
-            self.lbl_img.setPixmap(self.doubt)
+            self.userEmoticon.setPixmap(self.doubt)
         elif msg == "3":
-            self.lbl_img.setPixmap(self.tired)
+            self.userEmoticon.setPixmap(self.tired)
         elif msg == "4":
-            self.lbl_img.setPixmap(self.being_left)
+            self.userEmoticon.setPixmap(self.being_left)
         elif msg == "5":
-            self.lbl_img.setPixmap(self.yes)
+            self.userEmoticon.setPixmap(self.yes)
         elif msg == "6":
-            self.lbl_img.setPixmap(self.no)
+            self.userEmoticon.setPixmap(self.no)
 
 
 
@@ -96,53 +100,6 @@ class Worker(QThread):
 
         md = detector.MyDetector()
         md.video(self.state, self.sec_changed)
-        # print('load model...')
-        # detector = dlib.get_frontal_face_detector()
-        # # predictor = dlib.shape_predictor("public/shape_predictor_68_face_landmarks.dat")
-        #
-        # print('start video stream')
-        # vs = VideoStream(0).start()
-        # time.sleep(2.0)
-        #
-        # print('start detecting')
-        # while self.working:
-        #         frame = vs.read()
-        #         frame = imutils.resize(frame, width=400)
-        #         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        #
-        #         rects = detector(gray, 0)
-        #
-        #         # for rect in rects:
-        #             # shape = predictor(gray, rect)
-        #             # shape = face_utils.shape_to_np(shape)
-        #             #
-        #             # for (x, y) in shape:
-        #             #     cv2.circle(frame, (x, y), 1, (0, 0, 255), -1)
-        #
-        #         cv2.imshow("webcam", frame)
-        #         key = cv2.waitKey(1) & 0xFF
-        #
-        #         if key == ord("1"):
-        #             self.sec = 1
-        #             self.sec_changed.emit('{}'.format(self.sec))
-        #         if key == ord("2"):
-        #             self.sec = 2
-        #             self.sec_changed.emit('{}'.format(self.sec))
-        #         if key == ord("3"):
-        #             self.sec = 3
-        #             self.sec_changed.emit('{}'.format(self.sec))
-        #         if key == ord("4"):
-        #             self.sec = 4
-        #             self.sec_changed.emit('{}'.format(self.sec))
-        #         if key == ord("5"):
-        #             self.sec = 5
-        #             self.sec_changed.emit('{}'.format(self.sec))
-        #         if key == ord("6"):
-        #             self.sec = 6
-        #             self.sec_changed.emit('{}'.format(self.sec))
-        #
-        #         if key == ord("q"):
-        #             break
 
 
 
